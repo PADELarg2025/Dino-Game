20597a179806766fa4ad2474d9970ca856e748bc
Argie Padel <padelargie@gmail.com>
2025-01-23 15:43:37 +0800

    Initial Push
    - Added resources for the UI

---
+++ Assets/Sprite/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   public static GameManager Instance { get; private set; }
+
+    public float initialGameSpeed = 5f;
+    public float gameSpeedIncrease = 0.1f;
+    public float gameSpeed { get; private set; }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        } else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+    private void Start()
+    {
+        Newgame();
+    }
+
+    private void Newgame()
+    {
+        gameSpeed = initialGameSpeed;
+    }
+
+    private void Update()
+    {
+        gameSpeed += gameSpeed * Time.deltaTime;
+    }
+}
